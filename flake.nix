b4037b973c73489a8c12c211a4b13b192ef015ec
Vuk Urosevic <vuk23urosevic@gmail.com>
2025-03-02 22:42:15 +0100

    update

--- flake.nix
+++ flake.nix
@@ -4,19 +4,57 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
-  };
+    hyprland.url = "github:hyprwm/Hyprland";
+    stylix.url = "github:danth/stylix";
+    xremap-flake.url = "github:xremap/nix-flake";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    hyprland-plugins = {
+    	url = "github:hyprwm/hyprland-plugins";
+	inputs.hyprland.follows = "hyprland";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      host = "vukNixOS";
+      username = "vuk23";
+    in
+    {
+      nixosConfigurations = {
+        "${host}" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit system inputs username host;
+          };
+          modules = [
+            ./hosts/${host}/configuration.nix
+            #./modules/nixos
+            inputs.stylix.nixosModules.stylix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit system inputs username host;
+              };
+
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+
+              home-manager.users."${username}" = {
+                imports = [
+                  ./hosts/${host}/home.nix
+                  #./modules/home-manager
+                ];
+              };
+            }
+          ];
+        };
+      };
+    };
 }
