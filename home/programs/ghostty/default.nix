d935e15dfc79a051254e9cd264d5328b072bab6b
urosevicvuk <vuk23urosevic@gmail.com>
2025-05-06 21:36:00 +0200

    ghostty config

--- home/programs/ghostty/default.nix
+++ home/programs/ghostty/default.nix
@@ -2,13 +2,8 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      scrollback-lines = 10000;
-
       window-padding-x = 10;
-      window-padding-y = 5;
-
-      font-family = "JetBrainsMono Nerd Font";
-      font-size = 13;
+      window-padding-y = 10;
     };
   };
 }
