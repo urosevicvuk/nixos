54966ec676a4f3125d09efe38a0a57cd67ceca4c
urosevicvuk <vuk23urosevic@gmail.com>
2025-05-20 22:47:23 +0200

    added some needed packages to run clair obscur e33

--- hosts/anorLondo/home.nix
+++ hosts/anorLondo/home.nix
@@ -56,13 +56,18 @@
 
       #Gaming
       prismlauncher # Minecraft launcher
-      torzu # Switch emulator
       shadps4 # PS4 emulator
       lutris # Pirated game launcher
       (wineWowPackages.stable.override { waylandSupport = true; })
       winetricks
       protonup # Proton my beloved
       mangohud # FPS counter
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-libav
 
       # Dev
       go
