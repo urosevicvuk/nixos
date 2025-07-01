6a7ca7b3967871f6cfe071a0490e630144bb9734
urosevicvuk <vuk23urosevic@gmail.com>
2025-07-01 14:25:46 +0200

    test

--- modules/home/programs/git/default.nix
+++ modules/home/programs/git/default.nix
@@ -3,9 +3,11 @@
 let
   username = config.var.git.username;
   email = config.var.git.email;
-in {
+in
+{
   programs.git = {
     enable = true;
+    lfs.enable = true;
     userName = username;
     userEmail = email;
     ignores = [
@@ -46,12 +48,9 @@ in {
       st = "status";
       br = "branch";
       df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-      hist = ''
-        log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
-      llog = ''
-        log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
-      edit-unmerged =
-        "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      hist = ''log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
+      llog = ''log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
+      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
     };
   };
 }
