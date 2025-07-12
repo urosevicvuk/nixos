6cc5a442bd39ca7d8bf3dba453f53908af1b6565
urosevicvuk <vuk23urosevic@gmail.com>
2025-07-12 11:23:43 +0200

    minor tweaks

--- flake.nix
+++ flake.nix
@@ -6,31 +6,57 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+    };
 
-    ghostty.url = "github:ghostty-org/ghostty";
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+    };
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    };
 
-    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    hyprpanel = {
+      url = "github:Jas-SinghFSU/HyprPanel";
+    };
 
-    stylix.url = "github:danth/stylix";
+    stylix = {
+      url = "github:danth/stylix";
+    };
 
-    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
+    apple-fonts = {
+      url = "github:Lyndeno/apple-fonts.nix";
+    };
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+    };
 
-    nixcord.url = "github:kaylorben/nixcord"; # Discord client for NixOS
+    nixcord = {
+      url = "github:kaylorben/nixcord"; # Discord client for NixOS
+    };
 
-    sops-nix.url = "github:Mic92/sops-nix"; # SOPS integration for NixOS
+    sops-nix = {
+      url = "github:Mic92/sops-nix"; # SOPS integration for NixOS
+    };
 
-    nixarr.url = "github:rasmus-kirk/nixarr";
+    nixarr = {
+      url = "github:rasmus-kirk/nixarr";
+    };
 
-    anyrun.url = "github:fufexan/anyrun/launch-prefix";
+    anyrun = {
+      url = "github:fufexan/anyrun/launch-prefix";
+    };
 
-    nvf.url = "github:notashelf/nvf";
+    nvf = {
+      url = "github:notashelf/nvf";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -52,7 +78,9 @@
       inputs.hyprland.follows = "hyprland";
     };
 
-    search-nixos-api.url = "github:anotherhadi/search-nixos-api";
+    search-nixos-api = {
+      url = "github:anotherhadi/search-nixos-api";
+    };
   };
 
   outputs = inputs @ {nixpkgs, ...}: {
