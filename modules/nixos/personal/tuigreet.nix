4ed340eb2b8c308f11a45c2ff9ec758a9e0ae4d8
urosevicvuk <vuk23urosevic@gmail.com>
2025-08-25 14:17:09 +0200

    fixed minor evaluation error

--- modules/nixos/personal/tuigreet.nix
+++ modules/nixos/personal/tuigreet.nix
@@ -6,13 +6,13 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --asterisks --container-padding 2 --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --remember --asterisks --container-padding 2 --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
         user = "greeter";
       };
     };
   };
 
-  environment.systemPackages = with pkgs; [ greetd.tuigreet ];
+  environment.systemPackages = with pkgs; [ tuigreet ];
 
   # this is a life saver.
   # literally no documentation about this anywhere.
