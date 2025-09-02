df512d13c0800fb8a2718a65f7c099410afddd1c
urosevicvuk <vuk23urosevic@gmail.com>
2025-09-02 17:33:57 +0200

    removed uneeded tailscale packages for home-manager

--- modules/home-manager/programs/tailscale/default.nix
+++
@@ -1,7 +0,0 @@
-# Tailscale is a VPN service that works on top of WireGuard.
-# It allows me to access my servers and devices from anywhere.
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ tailscale tailscale-systray ];
-
-  # exec-once moved to main hyprland config to avoid conflicts
-}
