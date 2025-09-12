012ea34d5841c2908f2434dfa55c7044d6433d83
urosevicvuk <vuk23urosevic@gmail.com>
2025-09-12 11:24:57 +0200

    theme mods

--- modules/home-manager/programs/spicetify/default.nix
+++ modules/home-manager/programs/spicetify/default.nix
@@ -14,13 +14,8 @@ in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  stylix.targets.spicetify.enable = false;
-
   programs.spicetify = {
     enable = true;
-    theme = lib.mkForce spicePkgs.themes.dribbblish;
-
-    colorScheme = "custom";
 
     customColorScheme = {
       button = accent;
