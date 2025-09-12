72fc5a95eeb096eb9eb7070d29b26b1cae8571d1
urosevicvuk <vuk23urosevic@gmail.com>
2025-09-12 11:22:53 +0200

    spicetify setup

--- modules/home-manager/programs/spicetify/default.nix
+++ modules/home-manager/programs/spicetify/default.nix
@@ -1,44 +1,44 @@
 # Spicetify is a spotify client customizer
 {
-  #pkgs,
-  #config,
-  #lib,
+  pkgs,
+  config,
+  lib,
   inputs,
   ...
 }:
-#let
-#  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-#  accent = "${config.lib.stylix.colors.base0D}";
-#  background = "${config.lib.stylix.colors.base00}";
-#in
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  accent = "${config.lib.stylix.colors.base0D}";
+  background = "${config.lib.stylix.colors.base00}";
+in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  #stylix.targets.spicetify.enable = false;
+  stylix.targets.spicetify.enable = false;
 
   programs.spicetify = {
     enable = true;
-    #theme = lib.mkForce spicePkgs.themes.dribbblish;
+    theme = lib.mkForce spicePkgs.themes.dribbblish;
 
-    #colorScheme = "custom";
+    colorScheme = "custom";
 
-    #customColorScheme = {
-    #  button = accent;
-    #  button-active = accent;
-    #  tab-active = accent;
-    #  player = background;
-    #  main = background;
-    #  sidebar = background;
-    #};
+    customColorScheme = {
+      button = accent;
+      button-active = accent;
+      tab-active = accent;
+      player = background;
+      main = background;
+      sidebar = background;
+    };
 
-    #enabledExtensions = with spicePkgs.extensions; [
-    #  playlistIcons
-    #  lastfm
-    #  historyShortcut
-    #  hidePodcasts
-    #  adblock
-    #  fullAppDisplay
-    #  keyboardShortcut
-    #];
+    enabledExtensions = with spicePkgs.extensions; [
+      playlistIcons
+      lastfm
+      historyShortcut
+      hidePodcasts
+      adblock
+      fullAppDisplay
+      keyboardShortcut
+    ];
   };
 }
