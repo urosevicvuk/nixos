94424b6a6f28c139eb083d8116a2e4712a8f9263
urosevicvuk <vuk23urosevic@gmail.com>
2025-10-13 00:06:36 +0200

    refactoring nixos modules

--- hosts/ariandel/configuration.nix
+++ hosts/ariandel/configuration.nix
@@ -1,54 +1,40 @@
 { config, ... }:
 {
   imports = [
-    # Core modules
-    ../../modules/nixos/core/nix.nix
-    ../../modules/nixos/core/systemd-boot.nix
-    ../../modules/nixos/core/users.nix
-    ../../modules/nixos/core/utils.nix
-    ../../modules/nixos/core/home-manager.nix
-    ../../modules/nixos/core/networking.nix
-    ../../modules/nixos/core/locale.nix
-    ../../modules/nixos/core/environment.nix
-    ../../modules/nixos/core/security.nix
+    # System modules
+    ../../modules/nixos/system/nix.nix
+    ../../modules/nixos/system/systemd-boot.nix
+    ../../modules/nixos/system/users.nix
+    ../../modules/nixos/system/utils.nix
+    ../../modules/nixos/system/home-manager.nix
+    ../../modules/nixos/system/locale.nix
+    ../../modules/nixos/system/environment.nix
+    ../../modules/nixos/system/security.nix
+
+    # Hardware
+    ../../modules/nixos/hardware/audio.nix
+    ../../modules/nixos/hardware/bluetooth.nix
+    ../../modules/nixos/hardware/keyd.nix
+    ../../modules/nixos/hardware/peripherals.nix
 
     # Desktop environment
-    ../../modules/nixos/desktop/audio.nix
-    ../../modules/nixos/desktop/bluetooth.nix
-    # ../../modules/nixos/desktop/docker.nix
-    ../../modules/nixos/desktop/filesharing.nix
     ../../modules/nixos/desktop/fonts.nix
-    ../../modules/nixos/desktop/gaming.nix
     ../../modules/nixos/desktop/hyprland.nix
-    ../../modules/nixos/desktop/keyd.nix
     ../../modules/nixos/desktop/services.nix
     ../../modules/nixos/desktop/xdg.nix
     # ../../modules/nixos/desktop/sddm.nix
     ../../modules/nixos/desktop/tuigreet.nix
 
-    # Hardware
-    ../../modules/nixos/hardware/peripherals.nix
+    # Programs
+    ../../modules/nixos/programs/gaming.nix
+    ../../modules/nixos/programs/filesharing.nix
 
-    # Development tools
-    # ../../modules/nixos/development/virtualization.nix
+    # Services
+    # ../../modules/nixos/services/docker.nix
 
     # Network
+    ../../modules/nixos/network/networking.nix
     ../../modules/nixos/network/tailscale.nix
-
-    # Server
-    # ../../modules/nixos/server/ssh.nix
-    # ../../modules/nixos/server/firewall.nix
-    # ../../modules/nixos/server/web/nginx.nix
-    # ../../modules/nixos/server/media/arr.nix
-    # ../../modules/nixos/server/services/adguardhome.nix
-    # ../../modules/nixos/server/services/bitwarden.nix
-    # ../../modules/nixos/server/services/glance.nix
-    # ../../modules/nixos/server/services/headscale.nix
-    # ../../modules/nixos/server/services/hoarder.nix
-    # ../../modules/nixos/server/services/mealie.nix
-    # ../../modules/nixos/server/services/meilisearch.nix
-    # ../../modules/nixos/server/services/nextcloud.nix
-    # ../../modules/nixos/server/services/search-nixos-api.nix
 
     # Host-specific configuration
     ./hardware-configuration.nix
--- hosts/firelink/configuration.nix
+++ hosts/firelink/configuration.nix
@@ -1,42 +1,27 @@
 { config, ... }:
 {
   imports = [
-    # Core modules
-    ../../modules/nixos/core/nix.nix
-    ../../modules/nixos/core/systemd-boot.nix
-    ../../modules/nixos/core/users.nix
-    ../../modules/nixos/core/utils.nix
-    ../../modules/nixos/core/home-manager.nix
-    ../../modules/nixos/core/networking.nix
-    ../../modules/nixos/core/locale.nix
-    ../../modules/nixos/core/environment.nix
-    ../../modules/nixos/core/security.nix
+    # System modules
+    ../../modules/nixos/system/nix.nix
+    ../../modules/nixos/system/systemd-boot.nix
+    ../../modules/nixos/system/users.nix
+    ../../modules/nixos/system/utils.nix
+    ../../modules/nixos/system/home-manager.nix
+    ../../modules/nixos/system/locale.nix
+    ../../modules/nixos/system/environment.nix
+    ../../modules/nixos/system/security.nix
 
-    # Desktop environment
-    # ../../modules/nixos/desktop/audio.nix
-    # ../../modules/nixos/desktop/bluetooth.nix
-    # ../../modules/nixos/desktop/docker.nix
-    # ../../modules/nixos/desktop/fonts.nix
-    # ../../modules/nixos/desktop/hyprland.nix
-    # ../../modules/nixos/desktop/sddm.nix
-    # ../../modules/nixos/desktop/tuigreet.nix
-
-    # Development tools
-    ../../modules/nixos/development/docker.nix
-    # ../../modules/nixos/development/virtualization.nix
-    # ../../modules/nixos/development/tools.nix
-
-    # Gaming
-    # ../../modules/nixos/gaming/steam.nix
+    # Services
+    ../../modules/nixos/services/docker.nix
 
     # Network
+    ../../modules/nixos/network/networking.nix
     ../../modules/nixos/network/tailscale.nix
-    ../../modules/nixos/network/cloudflared.nix
+    ../../modules/nixos/network/firewall.nix
 
     # Server
     ../../modules/nixos/server/ssh.nix
-    ../../modules/nixos/server/firewall.nix
-    # ../../modules/nixos/server/media/arr.nix
+    ../../modules/nixos/server/services/cloudflared.nix
     ../../modules/nixos/server/services/nextcloud.nix
     # ../../modules/nixos/server/services/adguardhome.nix
     # ../../modules/nixos/server/services/bitwarden.nix
@@ -46,6 +31,7 @@
     # ../../modules/nixos/server/services/mealie.nix
     # ../../modules/nixos/server/services/meilisearch.nix
     # ../../modules/nixos/server/services/search-nixos-api.nix
+    # ../../modules/nixos/server/media/arr.nix
     ../../modules/nixos/server/web/nginx.nix
 
     # Host-specific configuration
