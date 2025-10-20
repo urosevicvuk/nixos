814c198cce4d7ae2ca14c9f984e2e221131da449
urosevicvuk <vuk23urosevic@gmail.com>
2025-10-20 17:42:41 +0200

    fixed evaluation warnings

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -9,20 +9,21 @@ in
 {
   home.file.".ssh/allowed_signers".text =
     "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQpgKiftVTzqkfu6zbRpvZFtWZH/HBQSj6DhuVvVRul vuk23urosevic@gmail.com";
+  
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      side-by-side = true;
+      line-numbers = true;
+      syntax-theme = "base16";
+    };
+  };
+
   programs.git = {
     enable = true;
     lfs.enable = true;
-    userName = username;
-    userEmail = email;
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        side-by-side = true;
-        line-numbers = true;
-        syntax-theme = "base16";
-      };
-    };
     ignores = [
       ".cache/"
       ".DS_Store"
@@ -35,41 +36,47 @@ in
       "result"
       "result-*"
     ];
-    extraConfig = {
+    settings = {
+      user = {
+        name = username;
+        email = email;
+        signingkey = "~/.ssh/id_ed25519.pub";
+      };
       init.defaultBranch = "master";
       pull.rebase = "false";
       push.autoSetupRemote = true;
       color.ui = "1";
       commit.gpgsign = true;
-      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-      gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
+      gpg = {
+        ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        format = "ssh";
+      };
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
-    };
-    aliases = {
-      essa = "push --force";
-      co = "checkout";
-      fuck = "commit --amend -m";
-      c = "commit -m";
-      ca = "commit -am";
-      forgor = "commit --amend --no-edit";
-      graph = "log --all --decorate --graph --oneline";
-      oops = "checkout --";
-      l = "log";
-      r = "rebase";
-      s = "status --short";
-      ss = "status";
-      d = "diff";
-      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-      af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
-      st = "status";
-      br = "branch";
-      df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-      hist = ''log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
-      llog = ''log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      alias = {
+        essa = "push --force";
+        co = "checkout";
+        fuck = "commit --amend -m";
+        c = "commit -m";
+        ca = "commit -am";
+        forgor = "commit --amend --no-edit";
+        graph = "log --all --decorate --graph --oneline";
+        oops = "checkout --";
+        l = "log";
+        r = "rebase";
+        s = "status --short";
+        ss = "status";
+        d = "diff";
+        ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+        pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+        af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
+        st = "status";
+        br = "branch";
+        df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
+        hist = ''log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
+        llog = ''log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
+        edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      };
     };
   };
 
