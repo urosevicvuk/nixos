4650170f56338c8816eda40072fc31582fcad88b
urosevicvuk <vuk23urosevic@gmail.com>
2025-11-08 02:51:48 +0100

    minor tweaks

--- hosts/ariandel/variables.nix
+++ hosts/ariandel/variables.nix
@@ -10,7 +10,7 @@
     configDirectory = "/home/" + config.var.username + "/nixos"; # The path of the nixos configuration directory
 
     device = "laptop"; # laptop || desktop || server
-    terminal = "kitty"; # default terminal emulator - kitty || ghostty
+    terminal = "ghostty"; # default terminal emulator - kitty || ghostty
 
     keyboardLayout = "us,rs,rs";
     keyboardVariant = ",latin,";
